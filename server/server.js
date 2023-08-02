37b13aaebcc9ffa7d21a2388c6553c4e6e9da05b
vishal <121763346+vishal-gg@users.noreply.github.com>
2023-08-02 16:14:12 +0530

    added cors support

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({extended: true}))
 
 
 app.use(cors({
-    origin: ["http://localhost:5173"]
+    origin: ["http://localhost:5173, https://e-com-admin-clientside.vercel.app"]
 }));
   
 // Routes
