b478b6d6303e04654b5d3c8c1a4d5426adbeff6b
vishal <121763346+vishal-gg@users.noreply.github.com>
2023-09-06 11:03:04 +0530

    trying second time to fix 404 error on page refresh

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './globals.css';
 import App from './App.tsx'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import store from './store.ts'
 
